c04c742c384e349675bf0cd55d6f27e047312f48
5ia17jowaeger <5ia17jowaeger@gibwintra.ch>
2019-01-14 08:27:15 +0100

    implement jre and made the first test

---
+++ src/test/java/ch/bbw/ConsoleCalculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package ch.bbw.ConsoleCalculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	Calculator testee;
+	@Test
+	public void testSummeZweiPositiveIsOk() {
+		testee = new Calculator();
+		assertTrue(testee.summe(10,25) == 35);
+	}
+
+}
