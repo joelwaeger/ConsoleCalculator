5d5e92b1c70af7dc54593d5db6f75656ea2f739e
joel <waegerjoel@gmail.com>
2019-01-20 20:47:59 +0100

    last changes

--- src/main/java/ch/bbw/ConsoleCalculator/Calculator.java
+++ src/main/java/ch/bbw/ConsoleCalculator/Calculator.java
@@ -14,5 +14,40 @@ public class Calculator {
 			
 		return value1 / value2;
 	}
+	int sum(int value1, int value2) {
+        long tester = (long) value1 + (long) value2;
+        overflowhandler(tester);
+        return (int) tester;
+    }
+
+    int subtractiontest(int value1, int value2){
+        long tester = (long)value1 - (long) value2;
+        overflowhandler(tester);
+        return (int)tester;
+    }
+
+    int multiplication(int value1, int value2){
+        long tester = (long) value1 * (long) value2;
+        overflowhandler(tester);
+        return (int) tester;
+    }
+
+    int divisiontest(int value1, int value2){
+        long tester = (long) value1 / (long) value2;
+        if (tester == 0){
+            throw new ArithmeticException("Result can't be 0");
+        }
+        return (int) tester;
+    }
+
+
+    private void overflowhandler(long tester) {
+        if (tester > Integer.MAX_VALUE) {
+            throw new ArithmeticException("Result is too big");
+        }
+        if (tester < Integer.MIN_VALUE) {
+            throw new ArithmeticException("Result is too small");
+        }
+    }
 }
  
--- src/test/java/ch/bbw/ConsoleCalculator/CalculatorTest.java
+++ src/test/java/ch/bbw/ConsoleCalculator/CalculatorTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 
 public class CalculatorTest {
 	Calculator testee;
-
+	
 	
 	@Before
 	public void setup(){
